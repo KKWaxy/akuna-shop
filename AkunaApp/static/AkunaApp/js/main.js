f9cc71267330210013ec37bafdfa4301ffdc0be5
kkwaxy <kkwaxy@gmail.com>
2020-12-19 13:39:44 +0000

    Je ne sais plus pour main.js

--- AkunaApp/static/AkunaApp/js/main.js
+++ AkunaApp/static/AkunaApp/js/main.js
@@ -1,21 +1,14 @@
 $(()=>{
   var akuna_window = $(window);
-
-    $("#carousel").carouFredSel(
-      {
-        items:1,
-        infinite:true,
-        circular:false,
-        align:"center",
-        scroll:{
-          items:1,
-          easing: "raise-in",
-          duration:500,
-          pauseOnHover:true
-        }
-      }
-    );
-    
+  $('.owl-carousel').owlCarousel(
+    {
+      items:1,
+      loop:true,
+      autoplay:true,
+      autoplayHoverPause:true,
+      center:true,
+    }
+  );
     akuna_window.on('scroll', () => {
         if(akuna_window.scrollTop() > 40){
             $('.akunaApp-header').addClass('sticky-top');
